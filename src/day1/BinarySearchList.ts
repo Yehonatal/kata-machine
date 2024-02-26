b129c617f0e77c16701a326b2b7e4e5283078cfa
yonatan afewerk <yonatanafewerk@gmail.com>
2024-02-26 10:35:44 +0300

    Search complete :)

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,18 +1,17 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-    let low = 0;
-    let high = haystack.length - 1;
+    let lo = 0;
+    let hi = haystack.length;
 
-    while (low <= high) {
-        const mid = Math.floor((low + high) / 2);
+    do {
+        let mid = Math.floor(lo + (hi - lo) / 2);
 
         if (haystack[mid] === needle) {
             return true;
-        } else if (haystack[mid] > needle) {
-            high = mid - 1;
+        } else if (needle > haystack[mid]) {
+            lo = mid + 1;
         } else {
-            low = mid + 1;
+            hi = mid;
         }
-    }
-
+    } while (lo < hi);
     return false;
 }
