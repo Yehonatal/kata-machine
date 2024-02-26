ce82f82af89980af96c87c018532f5c3078dc154
yonatan afewerk <yonatanafewerk@gmail.com>
2024-02-26 13:46:23 +0300

    Sort portion complete 🤧

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,46 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        const newNode = { value: item } as Node<T>;
+        this.length++;
+        if (!this.tail) {
+            this.tail = this.head = newNode;
+            return;
+        }
 
-}
-    deque(): T | undefined {
+        this.tail.next = newNode;
+        this.tail = newNode;
+    }
+    dequeue(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        this.length--;
+        const head = this.head;
+        this.head = this.head.next;
+
+        // freeing up the space and dealing with the tail
+        head.next = undefined;
+        if (this.length === 0) {
+            this.tail = undefined;
+        }
+        return head.value;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
--- src/day1/SinglyLinkedList.ts
+++ src/day1/SinglyLinkedList.ts
@@ -1,27 +1,139 @@
+class Node<T> {
+    public data: T;
+    public next: Node<T> | null;
+
+    constructor(data: T) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
 export default class SinglyLinkedList<T> {
     public length: number;
-
-    
+    public head: Node<T> | null;
 
     constructor() {
+        this.length = 0;
+        this.head = null;
     }
 
     prepend(item: T): void {
+        const newNode = new Node<T>(item);
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+    }
 
-}
     insertAt(item: T, idx: number): void {
+        if (idx < 0 || idx > this.length) {
+            throw new Error("Index out of range");
+        }
 
-}
+        if (idx === 0) {
+            this.prepend(item);
+            return;
+        }
+
+        const newNode = new Node<T>(item);
+        let current = this.head;
+        let prev = null;
+        let i = 0;
+
+        while (current && i < idx) {
+            prev = current;
+            current = current.next;
+            i++;
+        }
+
+        newNode.next = current;
+        if (prev) {
+            prev.next = newNode;
+        } else {
+            this.head = newNode;
+        }
+
+        this.length++;
+    }
+
     append(item: T): void {
+        const newNode = new Node<T>(item);
 
-}
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+
+        this.length++;
+    }
+
     remove(item: T): T | undefined {
+        let current = this.head;
+        let prev = null;
 
-}
+        while (current) {
+            if (current.data === item) {
+                if (prev) {
+                    prev.next = current.next;
+                } else {
+                    this.head = current.next;
+                }
+                this.length--;
+                return current.data;
+            }
+            prev = current;
+            current = current.next;
+        }
+
+        return undefined;
+    }
+
     get(idx: number): T | undefined {
+        if (idx < 0 || idx >= this.length) {
+            return undefined;
+        }
 
-}
+        let current = this.head;
+        let i = 0;
+
+        while (current && i < idx) {
+            current = current.next;
+            i++;
+        }
+
+        return current ? current.data : undefined;
+    }
+
     removeAt(idx: number): T | undefined {
+        if (idx < 0 || idx >= this.length) {
+            return undefined;
+        }
 
-}
+        let current = this.head;
+        let prev = null;
+        let i = 0;
+
+        while (current && i < idx) {
+            prev = current;
+            current = current.next;
+            i++;
+        }
+
+        if (prev) {
+            prev.next = current ? current.next : null;
+        } else {
+            this.head = current ? current.next : null;
+        }
+
+        if (current) {
+            this.length--;
+            return current.data;
+        }
+
+        return undefined;
+    }
 }
